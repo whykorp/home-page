1b5c2618debffc1c0059171e6bce3268cd83476d
WhyKorp <117651228+whykorp@users.noreply.github.com>
2023-12-19 22:40:33 +0100

    Remake

---
+++ tools/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+  <link href="style.css" rel="stylesheet">
+  <title>WhyTools</title>
+</head>
+<body>
+<div class="container">
+  <header>
+    <a href="javascript:history.back()" target="_blank">
+      <img class="logo" src="fluidsim/img/logo.png" alt="Logo">
+    </a>
+    <h1 class="title">WhyKorp</h1>
+  </header>
+  <div class="nav">
+    <a href="#">Accueil</a>
+    <a href="#">Infos</a>
+    <a href="https://artists.magroove.com/en/lt/whykioh/" target="_blank">Link Tree</a>
+	<a href="tools/index.html">Outils</a>
+	<a href="ruty/index.php">Ruty</a>
+	<a href="lolivator/index.php">LoLivator</a>
+	<a href="snt/index.html">SNT</a>
+	<a href="archives/index.php">Archives</a>
+  </div>
+  <p><br></p>
+  <hr>
+  <h1>Bienvenue sur WhykTools</h1>
+  <p>Voici la page des outils référencés</p>
+  <hr>
+  <a href="fluidsim">
+	<img  width="256px" src="fluidsim/preview.gif">
+  </a>
+  <a href="multipleWindow3dScene">
+	<img width="256px" src="multipleWindow3dScene/preview.png">
+  </a>
+</div>
+<div class="footer">
+  Copyright | Whykorp® 2021-2024
+</div>
+</body>
+</html>
