62793dc6225f2b5061522b98833d3bcec4c8f176
Whykorp <whykorp@gmail.com>
2024-02-18 20:14:51 +0100

    Add MikAMuz part

---
+++ mikamuz/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mik'A'Muz</title>
+</head>
+<body>
+<div class="container">
+  <header>
+    <a href="https://www.facebook.com/mikamuzdj" target="_blank">
+      <img class="logo" src="img/logo.png" alt="Logo">
+    </a>
+    <h1 class="title">Mik'A'Muz</h1>
+	<h4>DJ amateur depuis plus de 15 ans passionné de musique depuis petit, je me ferai un plaisir de vous aMUZer sur des MUZiques de tous styles.</h4>
+  </header>
+  <hr>
+  <h1>Liste de karaokés - 2024</h1>
+  <p>Vous pouvez télécharger la liste des karaokés entière. Ou effectuer une recherche dans la liste tout en bas.</p>
+  <div class="nav">
+  <a href="Liste karaoké 2024.pdf" target="_BLANK">Liste Karaoké 2024</a>
+  </div>
+  <br>
+  <hr>
+  <h1>Recherche dans la liste de karaoké</h1>
+  <input type="text" id="searchInput" placeholder="Rechercher...">
+  <ul id="results"></ul>
+</div>
+<div class="footer">
+  Copyright | Whykorp® 2021-2024
+</div>
+<script src="script.js"></script>
+</body>
+</html>
---
+++ mikamuz/script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.getElementById('searchInput');
+    const resultsList = document.getElementById('results');
+
+    // Charger le fichier CSV
+    fetch('Liste karaoké 2024.csv')
+        .then(response => response.text())
+        .then(data => {
+            const lines = data.split('\n');
+            const items = lines.map(line => line.trim());
+
+// Fonction de recherche
+function search(query) {
+    resultsList.innerHTML = '';
+    const filteredItems = items.filter(item => 
+        item.toLowerCase().includes(query.toLowerCase())
+    );
+    filteredItems.forEach(item => {
+        let displayedItem = item.replace('.mp4', '').replace(';;;', '');
+        const li = document.createElement('li');
+        li.textContent = displayedItem;
+        resultsList.appendChild(li);
+    });
+}
+
+
+            // Écouter les changements de la barre de recherche
+            searchInput.addEventListener('input', function () {
+                search(searchInput.value);
+            });
+        })
+        .catch(error => console.error('Erreur de chargement du fichier :', error));
+});
