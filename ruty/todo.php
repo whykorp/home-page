bac77cffddebae841b52439b80f415a002c3a238
WhyKorp <117651228+whykorp@users.noreply.github.com>
2024-03-11 19:32:46 +0100

    add another check button

--- ruty/todo.php
+++ ruty/todo.php
@@ -19,7 +19,7 @@
                 $taskName = $row["task_name"];
                 $completed = $row["completed"] ? "checked" : "";
 
-                echo "<li><input type='checkbox' $completed onchange='updateTask($taskId, this.checked)'> $taskName</li>";
+                echo "<li><div class='checkbox-wrapper'><input id='terms-checkbox-37' name='checkbox' type='checkbox'><label class='terms-label' for='terms-checkbox-37'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 200 200' class='checkbox-svg'><mask fill='white' id='path-1-inside-1_476_5-37'><rect height='200' width='200'></rect></mask><rect mask='url(#path-1-inside-1_476_5-37)' stroke-width='40' class='checkbox-box' height='200' width='200'></rect><path stroke-width='15' d='M52 111.018L76.9867 136L149 64' class='checkbox-tick'></path></svg>span class='label-text'>$taskName</span></label></div></li>";
             }
         } else {
             echo "Aucune tâche trouvée.";
