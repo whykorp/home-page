c6707b6182728d1e3fbb28e37cb6926a4230c050
Whykioh <whykioh@gmail.com>
2026-03-02 08:25:41 +0100

    Fix Delete Game

--- poker-paf/delete_game.php
+++ poker-paf/delete_game.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'db.php';
+header('Content-Type: application/json');
 
     // On récupère l'ID envoyé en POST, sinon on prend la dernière partie créée
     $game_id = isset($_SESSION['game_id']) ? $_SESSION['game_id'] : null;
